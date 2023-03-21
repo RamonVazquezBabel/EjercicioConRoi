561a6569a97516d4981ff6bba35fa7ff4b257e1f
Ramon Vazquez <ramon.vazquez@babelgroup.com>
2023-03-21 09:40:57 +0100

    first commit

---
+++ src/main/java/com/example/ejercicioConRoi/EjercicioConRoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioConRoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioConRoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioConRoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejercicioConRoi/EjercicioConRoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioConRoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioConRoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
